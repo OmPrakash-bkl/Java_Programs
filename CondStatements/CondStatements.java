8339764d90df096f2b96b445fe8bff85b61238bd
prakashz <mydreamzjobz@gmail.com>
2024-02-29 11:27:22 +0530

    added some of the notes

---
+++ CondStatements/CondStatements.java
@@ -0,0 +1,13 @@
+
+public class CondStatements {
+
+    public static void main(String[] args) {
+        int number = 15;
+        if(number % 2 == 0) {
+            System.out.println(number + " is Even");
+        } else {
+            System.out.println(number + " is Odd");
+        }
+    }
+    
+}
---
+++ VarAndDataTypes/VarAndDataTypes.java
@@ -0,0 +1,15 @@
+
+
+public class VarAndDataTypes {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        char gender = 'M';
+        boolean isStudent = true;
+
+        System.out.println("Age: "+age);
+        System.out.println("Height "+height);
+        System.out.println("Gender: "+gender);
+        System.out.println("Is Student: "+isStudent);
+    }
+}
